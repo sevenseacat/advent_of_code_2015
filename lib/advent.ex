4bbfd89f5d2424a71a5c7819b8b3e65fec228d93
Rebecca Le <traybaby@gmail.com>
2020-11-23 22:54:15 +0800

    Day 13, part 1

--- lib/advent.ex
+++ lib/advent.ex
@@ -1,3 +1,11 @@
 defmodule Advent do
   def data(day), do: File.read!("lib/data/day#{day}")
+
+  # http://www.petecorey.com/blog/2018/11/12/permutations-with-and-without-repetition-in-elixir/
+  def permutations([], _), do: [[]]
+  def permutations(_list, 0), do: [[]]
+
+  def permutations(list, k) do
+    for head <- list, tail <- permutations(list -- [head], k - 1), do: [head | tail]
+  end
 end
--- test/advent_test.exs
+++ test/advent_test.exs
@@ -12,4 +12,21 @@ defmodule AdventTest do
   doctest Day10
   doctest Day11
   doctest Day12
+
+  describe "Day13" do
+    test "can parse input" do
+      input = "Alice would gain 54 happiness units by sitting next to Bob.
+Alice would lose 79 happiness units by sitting next to Carol."
+      output = [{"Alice", "Bob", 54}, {"Alice", "Carol", -79}]
+
+      assert Day13.parse_input(input) == output
+    end
+
+    test "part1" do
+      input = File.read!("test/data/day13")
+      output = {[{"Alice", 52}, {"Bob", 76}, {"Carol", 115}, {"David", 87}], 330}
+
+      assert Day13.part1(input) == output
+    end
+  end
 end
